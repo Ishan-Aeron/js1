b19ada3eb2b62e02668411625821e595028c4d02
Ishan-Aeron <ishanaeron72@gmail.com>
2025-12-11 06:59:24 +0000

    datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict "; //treat all JS code as newer version
+
+
+//alert("Hello") //we are using nodejs, not browser
+let state=null
+console.log(3); 
+console.log("Hitesh");
+// number => 2^53
+// bigint
+//  string="" ''
+//  boolean
+//  null standalone value 
+//  undefined=> value not defined 
+// symbol=>unique
+
+/*
+    object
+    
+*/
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
