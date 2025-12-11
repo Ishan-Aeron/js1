5d027f7e7c20b4b323c6ae449c5c17202b7bbae3
Ishan-Aeron <ishanaeron72@gmail.com>
2025-12-11 14:21:03 +0000

    date commit

--- 01_basic/06_nums_and_maths.js
+++ 01_basic/06_nums_and_maths.js
@@ -1,8 +1,22 @@
 const score=400
 
 const balance =new Number(100);
-console.log(balance); //[Number: 100]
+// console.log(balance); //[Number: 100]
 
 
-console.log(balance.toString())
-console.log(balance.toFixed(3))
+// console.log(balance.toString())
+// console.log(balance.toFixed(3))
+
+// console.log(balance.toPrecision(4));
+
+const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++++++MATHS++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+
+console.log(Math.round(5.5));
+
+
---
+++ 01_basic/07_date_in_js.js
@@ -0,0 +1,16 @@
+let myDate=new Date()
+/*
+console.log(myDate.toString());//Thu Dec 11 2025 14:09:43 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toDateString());//Thu Dec 11 2025
+console.log(typeof myDate);//object
+*/
+
+//let myCreatedDate=new Date(2023,0,23)
+//let myCreatedDate=new Date("2023-01-14");
+let myCreatedDate=new Date("01-14-2023");
+console.log(myCreatedDate.toDateString());
+
+let myTimeStamp= Date.now();
+//console.log(myTimeStamp)
+//console.log(myCreatedDate.getTime())
+// 
