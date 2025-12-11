5d9f7b70a77c712e4752e7f5a365efca01e18e26
Ishan-Aeron <ishanaeron72@gmail.com>
2025-12-11 06:45:54 +0000

    02

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,16 @@
+const accountId =144553
+let accountEmail="ishanaeron72@gmail.com"
+var accountPassword="123456"
+accountCity="Jaipur"
+
+// accountId=2 
+
+/*
+
+    var no scope issue in block scope and functional scope
+    let scope
+
+*/
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
