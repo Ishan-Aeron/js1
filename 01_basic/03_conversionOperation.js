52a6fb5a9b50718679fdf05fe3be33d5ca291814
Ishan-Aeron <ishanaeron72@gmail.com>
2025-12-11 07:10:58 +0000

    conversion of Data types

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,24 @@
+// let score= undefined
+
+// console.log(typeof score);
+
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+ 
+/*
+    "33"=>33
+    "33abc"=> NaN
+    true=> 1; false=>0
+*/
+let isLoggedIN= "Ishan"
+
+let booleanIsLoggedIn=Boolean(isLoggedIN)
+console.log(booleanIsLoggedIn);
+
+/* 
+    1=> true;0=>false
+    "" => false;
+    " "=> true;
+*/
+console.log(String(booleanIsLoggedIn));
